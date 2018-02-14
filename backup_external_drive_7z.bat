ede5e1545d126c049d363fdd39db077cabbf7e47
Mike Clack Jones <mjclackjones@gmail.com>
2018-02-14 13:33:45 +0000

    Add backup script

---
+++ backup_external_drive_7z.bat
@@ -0,0 +1,19 @@
+if not exist Y:\ (
+	msg "%username%" Y: drive is not mounted, please check your config and try again
+)
+
+if exist Y:\ (
+	REM update archive - copy all files from disk and don't purge those deleted on disk
+
+	if exist Y:\secure.7z (
+		7z u -up0q1r2x2y2w2!Y:\secure.7z %HOME_DIR% -p%BACKUP_PASSWORD%
+	)
+
+	if not exist Y:\secure.7z (
+		msg "%username%" Archiving
+		7z a C:\Backup\secure.7z %HOME_DIR% -p%BACKUP_PASSWORD%
+		ROBOCOPY C:\Backup Y:\
+	)
+
+	msg "%username%" Backup to Y: successful
+)
