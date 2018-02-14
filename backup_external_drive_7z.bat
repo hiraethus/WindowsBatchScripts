4186eedf791ec28ec2f4c93f305a8b74e783c625
Mike Clack Jones <mjclackjones@gmail.com>
2018-02-14 13:43:00 +0000

    Update backup_external_drive_7z.bat

--- backup_external_drive_7z.bat
+++ backup_external_drive_7z.bat
@@ -6,12 +6,12 @@ if exist Y:\ (
 	REM update archive - copy all files from disk and don't purge those deleted on disk
 
 	if exist Y:\secure.7z (
-		7z u -up0q1r2x2y2w2!Y:\secure.7z %HOME_DIR% -p%BACKUP_PASSWORD%
+		7z u -up0q1r2x2y2w2!Y:\secure.7z %HOMEDRIVE%%HOMEPATH% -p%BACKUP_PASSWORD%
 	)
 
 	if not exist Y:\secure.7z (
 		msg "%username%" Archiving
-		7z a C:\Backup\secure.7z %HOME_DIR% -p%BACKUP_PASSWORD%
+		7z a C:\Backup\secure.7z %HOMEDRIVE%%HOMEPATH% -p%BACKUP_PASSWORD%
 		ROBOCOPY C:\Backup Y:\
 	)
 
